09749c21b5477ed611f2776950553a4b1db57a9b
Tyuiu.ChigurovMV@outlook.com <maxchigur@mail.ru>
2024-09-24 23:44:33 +0500

    Task1

--- Tyuiu.ChigurovMV.Sprint2.Task1.V23/Program.cs
+++ Tyuiu.ChigurovMV.Sprint2.Task1.V23/Program.cs
@@ -7,11 +7,16 @@ namespace Tyuiu.ChigurovMV.Sprint2.Task1.V23
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-
+            int a = 242;
+            int b = 571;
+            int c = 325;
+            int d = 155;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
             Console.Title = "Спринт #2 | Выполнил: Чигуров М. В. | ПКТб-24-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #2                                                               *");
-            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Тема: Логически операции                                                *");
             Console.WriteLine("* Задание #1                                                              *");
             Console.WriteLine("* Вариант #23                                                             *");
             Console.WriteLine("* Выполнил: Чигуров Максим Владиславович | ПКТб-24-1                      *");
@@ -23,6 +28,17 @@ namespace Tyuiu.ChigurovMV.Sprint2.Task1.V23
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* A = " + a);
+            Console.WriteLine("* B = " + b);
+            Console.WriteLine("* C = " + c);
+            Console.WriteLine("* D = " + d);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
 
         }
     }
