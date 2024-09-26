ab334be45c902413a191875c815d78877d5a5315
Tyuiu.ChigurovMV@outlook.com <maxchigur@mail.ru>
2024-09-26 18:33:15 +0500

    Task5

---
+++ Tyuiu.ChigurovMV.Sprint2.Task5.V15.Lib/DataService.cs
@@ -0,0 +1,39 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ChigurovMV.Sprint2.Task5.V15.Lib
+{
+    public class DataService : ISprint2Task5V15
+
+    {
+        public string FindDayName(int k)
+        {
+            string res;
+            res = "";
+            switch (k % 7)
+            {
+                case 0:
+                    res = "Воскресенье";
+                    break;
+                case 1:
+                    res = "Понедельник";
+                    break;
+                case 2:
+                    res = "Вторник";
+                    break;
+                case 3:
+                    res = "Среда";
+                    break;
+                case 4:
+                    res = "Четверг";
+                    break;
+                case 5:
+                    res = "Пятница";
+                    break;
+                case 6:
+                    res = "Суббота";
+                    break;   
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ChigurovMV.Sprint2.Task5.V15/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.ChigurovMV.Sprint2.Task5.V15.Lib;
+
+namespace Tyuiu.ChigurovMV.Sprint2.Task5.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int k;
+            Console.Title = "Спринт #2 | Выполнил: Чигуров М. В. | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил: Чигуров Максим Владиславович | ПКТб-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет и      *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Введите день k: ");
+            k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (k >= 1 && k <= 365)
+            {
+                Console.WriteLine(ds.FindDayName(k));
+            }
+            else
+            {
+                Console.WriteLine("* День находится вне границ! ");
+            }
+        }
+    }
+}
